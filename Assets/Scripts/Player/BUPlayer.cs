2316ef4c29b1d1eb6fd0c474e04b799aa224b3f2
Khánh <77840594+sshinichi-69@users.noreply.github.com>
2022-11-29 02:36:50 +0700

    merge

--- Assets/Scripts/Player/BUPlayer.cs
+++ Assets/Scripts/Player/BUPlayer.cs
@@ -41,7 +41,7 @@ public class BUPlayer : MonoBehaviour
         
     }
 
-    public void init(BrickColor color, string name, string myName, bool isAI)
+    public void init(BrickColor color, string name, string myName, bool isAI, int depth = 1)
     {
         this.color = color;
         this.name = name;
@@ -59,27 +59,7 @@ public class BUPlayer : MonoBehaviour
         if (this.isAI)
         {
             this.AddComponent<AI>();
-            GetComponent<AI>().init(color);
-        }
-    }
-
-    public void init(BrickColor color, BrickColor myColor, bool isAI)
-    {
-        this.color = color;
-        isPassed = false;
-        if (myColor == color)
-        {
-            isMyPlayer = true;
-        }
-        else
-        {
-            isMyPlayer = false;
-        }
-        this.isAI = isAI;
-        if (this.isAI)
-        {
-            this.AddComponent<AI>();
-            GetComponent<AI>().init(color);
+            GetComponent<AI>().init(color, depth);
         }
     }
 
